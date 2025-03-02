fd8b3c511d20ad2a672c39c521238609713c6ad4
JohnWilliam24dev <johnwilliam24.dev@gmail.com>
2025-03-02 12:01:24 -0300

    correção install

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 set -e  # Faz o script falhar se algum comando falhar
 
 echo "Instalando PostGuy..."
-pip install --upgrade git+https://github.com/JohnWilliam24dev/postguy.git
 
-echo "Instalação concluída!"
+pip install --user --upgrade git+https://github.com/JohnWilliam24dev/postguy.git
+
+# Adiciona ~/.local/bin ao PATH se necessário
+if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+echo "Instalação concluída! Tente rodar 'postguy --help'"
